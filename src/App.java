e3c9d0855450fa8ce6c968a8168e8eb51ebb9269
kerrrusha <brcyrill2@gmail.com>
2022-06-12 01:21:58 +0300

    update 12.06.2022

--- src/App.java
+++
@@ -1,9 +0,0 @@
-import controller.*;
-
-import java.io.IOException;
-
-public class App {
-    public static void main(String[] args) throws IOException, ClassNotFoundException {
-        new AirportController().run();
-    }
-}
